1240c3095f510bee0cd1f7b16c7ee86a32902663
khaylah <khaylahrscholar@gmail.com>
2022-11-25 23:11:44 -0600

    updates homePage

---
+++ src/components/homepartThree.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function homepartThree() {
+  return (
+    <>
+    <div className='containThree'>
+        <p id="textThree">What Customers Say About Turner's Cleaning Services</p>
+        <div className='reviewBox'></div>
+    </div>
+    <div className='space'></div>
+    
+
+    </>
+  )
+}
+
+export default homepartThree
