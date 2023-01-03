8689afdeae8d36990566a8a0ecd304e9d3c44461
khaylah <khaylahrscholar@gmail.com>
2023-01-02 23:19:06 -0500

    Fixed Gallery

---
+++ src/components/g.js
@@ -0,0 +1,26 @@
+import $ from "jquery"
+import tur from '../img/tur.png'
+
+const buttons = document.querySelectorAll('.project');
+const overlay = document.querySelector('.overlay');
+const overlayImage = document.querySelector('.overlay__inner img');
+
+function open(e) {
+  overlay.classList.add('open');
+  const src= e.currentTarget.querySelector('img').src;
+  overlayImage.src = src;
+}
+
+function close() {
+  overlay.classList.remove('open');
+}
+
+buttons.forEach(button => button.addEventListener('click', open));
+
+//overlay.addEventListener('click', close);
+if (overlay) {
+    // Not called
+    overlay.addEventListener('click', () => {
+        overlay.classList.remove('open');
+    });
+  }
